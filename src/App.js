740d4b44194ccbf836af21040c6ad5f24d93f315
Naman Maurya <namanmaurya000@gmail.com>
2023-09-28 19:16:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css";
+import Converter from './Components/Converter'
+
+function App() {
+  return (
+    <div className='App'><Converter/></div>
+  )
+}
+
+export default App
